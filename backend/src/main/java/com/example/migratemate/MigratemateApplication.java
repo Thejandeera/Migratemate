f65776e7fe9262048f687e01a56318995e03f36d
Thejandeera <thejan1010@gmail.com>
2026-01-10 02:38:06 +0530

    initial file setup

---
+++ backend/src/main/java/com/example/migratemate/MigratemateApplication.java
@@ -0,0 +1,13 @@
+package com.example.migratemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigratemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigratemateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/migratemate/MigratemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.migratemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigratemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
